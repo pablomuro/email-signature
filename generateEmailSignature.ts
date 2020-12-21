2333b6bf083e4ea58859345132336c2f229403f7
Pablo A. Muro Martinez <pablo.muro@gmail.com>
2020-12-20 23:01:16 -0300

    refactor - all html stuff on generator class

--- generateEmailSignature.ts
+++ generateEmailSignature.ts
@@ -1,39 +1,86 @@
 import juice from 'juice';
 import fs from 'fs';
+import path from 'path'
+import sass from 'node-sass'
 import {
   getCheerioEmailTemplate, getThemeColorsMap, replaceVarsForColorAndPutCdnOnUrls,
   generatePngFromSvgFiles,
   generateBackgroundPng, changeSvgImgSourceFromHtml
 } from './utils'
+import { createSSRApp } from 'vue'
+import { renderToString } from '@vue/server-renderer'
+import dotenv from 'dotenv'
+dotenv.config()
 
 
 const IS_TO_COMMIT_FILES = false
 
 
 class GenerateEmailSignature {
-  GIT_CDN_NAME = 'https://ghcdn.rawgit.org/'
-  REPOSITORY = 'pablomuro/email-signature/main/'
   BACKGROUND_SIZE = 35
   DEFAULT_THEME_COLOR = '#34495E'  //azul-default: #6192de    azul-escuro: #34495E
-  OVERRIDE_CSS_THEME_COLOR = false
+
+  gitCdnName = 'https://ghcdn.rawgit.org/'
+  gitRepository = 'pablomuro/email-signature/main/'
+  templatePath = path.join(__dirname, 'signature_templates')
+  defaultTemplateFile = path.join(this.templatePath, 'index.html')
 
   _html: string | null;
   themeColor: string | null;
-  cdnUrl = this.GIT_CDN_NAME + this.REPOSITORY
+  cdnUrl = this.gitCdnName + this.gitRepository
 
   constructor() {
     this._html = null;
     this.themeColor = null;
   }
 
-  init(html: string) {
-    this._html = html
-    const matchGroup = html.match(/(--theme-color)(.*)(#.*)(;)/m)
+  templateData() {
+    const {
+      name, profession, company, location, phone, email,
+      website_url, linkedin_url, github_url, twitter_url, instagram_url
+    } = process.env
+    return {
+      name, profession, company, location, phone, email,
+      website_url, linkedin_url, github_url, twitter_url, instagram_url
+    }
+  }
+
+  async init(fileName: string | null = null) {
+    let templateHtml: string
+    let templateCss: string
+
+    try {
+      templateHtml = fs.readFileSync(path.join(this.templatePath, `${fileName}.html`), 'utf-8').toString()
+      templateCss = fs.readFileSync(path.join(this.templatePath, `${fileName}.scss`), 'utf-8').toString()
+    } catch (error) {
+      templateHtml = fs.readFileSync(this.defaultTemplateFile, 'utf-8').toString()
+      templateCss = fs.readFileSync(this.defaultTemplateFile.replace('html', 'scss'), 'utf-8').toString()
+    }
+
+    const sassResult = sass.renderSync({
+      data: templateCss
+    });
+
+    templateCss = sassResult.css.toString()
+
+    const vueApp = createSSRApp({
+      data: () => ({ templateCss, ...this.templateData() }),
+      template: templateHtml
+    })
+
+    const renderHtml = await renderToString(vueApp)
+
+    generateEmailSignature.createImageFiles()
+
+    const matchGroup = renderHtml.match(/(--theme-color)(.*)(#.*)(;)/m)
     if (matchGroup && matchGroup[0].includes('--theme-color') && matchGroup[0].includes('#')) {
       this.themeColor = matchGroup[3]
     }
 
     this.createImageFiles()
+    this._html = renderHtml
+
+    return renderHtml
   }
 
   get html() {
@@ -47,7 +94,7 @@ class GenerateEmailSignature {
     const $ = getCheerioEmailTemplate(this._html)
     const cssText = $('html head style').contents().get(0).data
     const colorsMap = getThemeColorsMap(cssText)
-    const themeColor = (!this.OVERRIDE_CSS_THEME_COLOR) ? (colorsMap.get('--theme-color')) ? colorsMap.get('--theme-color') : this.DEFAULT_THEME_COLOR : this.DEFAULT_THEME_COLOR
+    const themeColor = (colorsMap.get('--theme-color')) ? colorsMap.get('--theme-color') : this.DEFAULT_THEME_COLOR
 
     const newCss = replaceVarsForColorAndPutCdnOnUrls(cssText, colorsMap, this.cdnUrl)
 
--- server.ts
+++ server.ts
@@ -1,53 +1,16 @@
 import express from 'express'
-import fs from 'fs'
 import path from 'path'
 import { generateEmailSignature } from './generateEmailSignature'
-import { createSSRApp } from 'vue'
-import { renderToString } from '@vue/server-renderer'
-import dotenv from 'dotenv'
-dotenv.config()
 
 const server = express()
 const port = 3000
 
-const templateData = function () {
-  const {
-    name, profession, company, location, phone, email,
-    website_url, linkedin_url, github_url, twitter_url, instagram_url
-  } = process.env
-  return {
-    name, profession, company, location, phone, email,
-    website_url, linkedin_url, github_url, twitter_url, instagram_url
-  }
-}
-
-const templatePath = path.join(__dirname, 'signature_templates')
-const defaultTemplateFile = path.join(templatePath, 'index.html')
-
 server.use('/img', express.static(path.join(__dirname, 'img')))
 
-let templateHtml: string
+
 const generateTemplateHtml = async (fileName: string | null = null) => {
 
-  if (fileName) {
-    try {
-      templateHtml = fs.readFileSync(path.join(templatePath, `${fileName}.html`), 'utf-8').toString()
-    } catch (error) {
-      templateHtml = fs.readFileSync(defaultTemplateFile, 'utf-8').toString()
-    }
-  } else {
-    templateHtml = fs.readFileSync(defaultTemplateFile, 'utf-8').toString()
-  }
-
-  const vueApp = createSSRApp({
-    data: () => ({ ...templateData() }),
-    template: templateHtml
-  })
-
-  const renderHtml = await renderToString(vueApp)
-
-  generateEmailSignature.init(renderHtml)
-  generateEmailSignature.createImageFiles()
+  const renderHtml = await generateEmailSignature.init(fileName)
 
   return renderHtml
 }
